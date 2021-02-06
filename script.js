7aa44594ca5de1aa5f1f698c2a9af0203d395fa9
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-02-05 19:06:32 -0500

    adding quote api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Get quotes from API
 
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log('No quote', error);
+    }
+}
+
+getQuote();
