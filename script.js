c8eb79a0af6bc8ad60e4d3cce047c4c7ff0654e0
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-02-05 22:48:03 -0500

    added functions, api, and event listeners

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
-// Get quotes from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+let apiQuotes = [];
+
+// Loading spinner shown
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Remove loading spinner
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+    loading();
+    // Pick random quote from array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+      } else {
+        quoteText.classList.remove('long-quote');
+      }
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get quotes from API
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com';
-    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiURL = 'https://type.fit/api/quotes';
     try {
-        const response = await fetch(proxyUrl + apiURL);
-        const data = await response.json();
-        console.log(data);
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
     } catch (error) {
-        getQuote();
-        console.log('No quote', error);
+        //catch error here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuote();
